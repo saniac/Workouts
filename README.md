2ecfbada9d8ae34914ede638df07c739638b6245
Stephen Judd <stephenj@catalyst.net.nz>
2013-12-23 17:35:04 +1300

    Tidying
    - added notes to README
    - made to compile to command line app

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Workouts
 ========
 
-Some Haskell code to calculate weights for a series of workouts
+Some Haskell code to calculate weights for a series of workouts, tailored to the actual free weights available.
+
+Goals are:
+
+- a command line app for calculating Wendler 5 3 1 workouts
+- ditto for any arbitrary linear progression
+- a module of functions for a Yesod application
+
+Compile with
+
+    ghc -main-is Workouts workout.hs
--- workout.hs
+++ workout.hs
@@ -17,11 +17,8 @@
 module Workouts
 (
  roundToIncrement
- , wendlerWeek
- , weekOne
- , weekTwo
- , weekThree
- , weekFour
+ , wendlerMonth
+ , main
 )
 where
 
@@ -31,22 +28,19 @@ import Data.List
 main = do
     args <- getArgs
     progName <- getProgName
-    case args of
-        [goal, workouts, weights] -> putStrLn (message goal workouts weights)
-        _ -> putStrLn ("Usage: " ++ progName ++ " goal workout weights")
+    case progName of 
+        "workout" -> case args of
+            [goal, workouts, weights] -> putStrLn (message goal workouts weights)
+            _ -> putStrLn ("Usage: " ++ progName ++ " goal workout_count available_weights")
+        "wendler" -> case args of
+            [increment, oneRM] -> putStrLn (show $ wendlerMonth (read increment :: Float) (read oneRM :: Float))
+            _ -> putStrLn ("Usage: " ++ progName ++ " available_increment 1RM")
         
 message goal workouts weights = show (workoutCycle g wo wg)
     where g  = read goal :: Float
           wo = read workouts :: Float
           wg = map read (words weights)::[Float] 
         
-
--- subsequences is not in Ubuntu Jaunty's version of GHC, unfortunately.
---subsequences xs = [] : subsequences' xs
---       where subsequences' []     = []
---             subsequences' (x:xs) = [x] : concatMap (\ys -> [ys, x:ys])
---                                                    (subsequences' xs)
-
 steps start end workouts = let step = ((end-start)/(workouts-1)) 
                            in [start, start + step .. end]
 
@@ -74,11 +68,14 @@ workoutCycle goal workouts weights = map (roundToIncrement increment) (steps sta
                               start      = bestStart  workouts goal increments
                               increments = tail (map (foldl (+) 0) (subsequences weights ))
 
+-- TODO There must be a neater way of doing this than going mappity mappity
+
 weekOne = [0.65, 0.75, 0.85]
 weekTwo = [0.7, 0.8, 0.9]
 weekThree = [0.75, 0.85, 0.95]
 weekFour = [0.4, 0.5, 0.6]
 
+wendlerWeek :: Float -> Float -> [Float] -> [Float]
 wendlerWeek increment oneRM week = map (roundToIncrement increment) $ map (* oneRM) week
 
 wendlerMonth inc oneRM = map (wendlerWeek inc oneRM) [weekOne, weekTwo, weekThree, weekFour]
